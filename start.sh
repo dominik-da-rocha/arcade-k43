59d9763fdb183f3532a1b3b8e5f753326e3737a4
Rocha <dominik.da.rocha@gmail.com>
2022-12-09 02:38:29 +0100

    added docker build

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 version="$(git describe --tags --dirty | sed 's/-/./g')"
+image="dominikdarocha/arcarde-k43"
 echo "// ---------------------------------------------------------------" >"version.go"
 echo "// generated file to not edit" >>"version.go"
 echo "// ---------------------------------------------------------------" >>"version.go"
@@ -7,10 +8,16 @@ echo "package main" >>"version.go"
 echo "const Version = \"$version\"" >>"version.go"
 echo "" >>"version.go"
 
-sed -i "s/AK43_VERSION=.*/AK43_VERSION=$version/g" .env
-
 if [[ "$1" == "run" ]]; then
    go run main.go version.go
+elif [[ "$1" == "docker" ]]; then
+   docker-compose down
+   docker image rm $(docker images -q $image)
+   docker-compose up -d
+   docker image tag "$image" "$image:$version"
+   docker image ls
+   docker push "$image"
+   docker push "$image:$version"
 else
    echo "unknown command $1"
 fi
